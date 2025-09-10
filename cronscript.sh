ab5e5c89435d68b2d5fa686969a114758184610e
Greg Baker <gregb@.ifost.org.au>
2025-09-10 17:59:35 +1000

    Run sentence analysis.

--- cronscript.sh
+++ cronscript.sh
@@ -9,5 +9,6 @@ uv run translate_pausanias.py --stop 50
 uv run split_sentences.py --stop 5
 uv run find_predictors.py
 #uv run analyse_noun_network.py 
+uv run sentence_mythic_sceptic_analyser.py --stop 25
 uv run create_website.py
 rsync -avz pausanias_site/ merah:/var/www/vhosts/pausanias.symmachus.org/htdocs/
